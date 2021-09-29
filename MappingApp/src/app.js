b59ead861764b29d8c4270b77f8437da567b452a
Jawad Shani <jawad.shani@hitachi-eu.com>
2021-09-29 13:45:49 +0200

    app updated

--- MappingApp/src/app.js
+++ MappingApp/src/app.js
@@ -6,7 +6,8 @@ var map = new ol.Map({
       })
     ],
     view: new ol.View({
-      center: ol.proj.fromLonLat([37.41, 8.82]),
+      // center: ol.proj.fromLonLat([37.41, 8.82]),
+      center: ol.proj.fromLonLat([-95, 45]), //USA
       zoom: 4
     })
   });
