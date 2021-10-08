e2389239e025e628ff2b3ff816f62bc2230b9347
Jawad Shani <jawad.shani@hitachi-eu.com>
2021-10-09 01:59:16 +0200

    map interactivity demos

--- MappingApp/src/map-handler.js
+++ MappingApp/src/map-handler.js
@@ -8,6 +8,12 @@ var MapHandler = (function () {
         this.layerData = new LayerData(this.truckDataHandlerArray);
     }
 
+
+    MapHandler.prototype.Test = function () {
+        var truckDataHandler = this.truckDataHandlerArray[0];
+        truckDataHandler.updateTruckPosition(this.layerData, 52.533976, 13.156240);
+     }
+
     MapHandler.prototype.loadTruckMapJsonData = function (json) {
         for (var tr = 0; tr < json.data.length; tr++) {
             var truckJson = json.data[tr];
@@ -45,10 +51,86 @@ var MapHandler = (function () {
 
         // Set the view for the map
         this.map.setView(view);  
+        var inst = this;
+
+        this.map.on('click', function (evt) {
+            inst.onClick(evt.pixel);
+         });
+
+         this.map.on('pointermove', function (evt) {
+            if (evt.dragging) {
+               info.tooltip('hide')
+               return;
+            }
+            var pixel = inst.map.getEventPixel(evt.originalEvent);
+            inst.onMouseOver(pixel);
+         });
 
     };
-
    
+    MapHandler.prototype.onClick = function (pixel) {
+        var inst = this;
+        var feature = this.map.forEachFeatureAtPixel(pixel, function (feature, layer) {
+           console.log("Click");
+           return feature;
+        });
+     };
+  
+
+     MapHandler.prototype.onMouseOver = function (pixel) { 
+        var inst = this;
+        var text;
+
+        var feature = this.map.forEachFeatureAtPixel(pixel, function (feature, layer) {
+           return feature;
+        });
+
+        if (feature) {
+            var properties = feature.getProperties()
+            if (properties.type == "tr") {
+               console.log(properties);
+               text = properties.registrationNumber;
+            }
+            else if (properties.type == "ts") {
+               console.log("TS: " + properties.timestamp);
+               text = properties.timestamp;
+            }
+            else if (properties.type == "po") {
+                console.log('po');
+                var truckDataHolder = this.findTruckById(properties.id);
+                this.layerData.buildTruckTrackLayers(truckDataHolder);
+                //text = properties.registrationNumber;
+             }          
+         }      
+
+         if(text) {
+             console.log('text: ', text);
+            info.css({
+               left: pixel[0] + 'px',
+               top: (pixel[1] - 15) + 'px'
+            });
+            info.tooltip('hide')
+             .attr('data-original-title', text)
+             .tooltip('fixTitle')
+             .tooltip('show');
+         } else {
+            info.tooltip('hide');
+         }
+   
+        //console.log(pixel);
+     };
+
+
+     MapHandler.prototype.findTruckById = function (id) {
+        for (var i = 0; i < this.truckDataHandlerArray.length; i++) {
+           var truckDataHandler = this.truckDataHandlerArray[i];
+           if (truckDataHandler.id == id)
+              return truckDataHandler;
+        }
+        return null;
+     };
+  
+
     return MapHandler;
 
 })();
@@ -147,6 +229,28 @@ var TruckDataHandler = (function () {
         this.currentPosition = lastCoordinates;
     };
 
+   TruckDataHandler.prototype.updateTruckPosition = function (layerData, lat, lon) {
+      var coordinates = ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857');
+      var rotation = 0;
+      if (this.currentPosition != null) {
+         var dx = coordinates[0] - this.currentPosition[0];
+         var dy = coordinates[1] - this.currentPosition[1];
+         rotation = Math.atan2(dy, dx);
+      }
+
+      layerData.truckPositionLayer.getSource().removeFeature(this.truckPositionFeature);
+      this.truckPositionFeature = this.createTruckPositionMarker(coordinates, rotation);
+      layerData.truckPositionLayer.getSource().addFeature(this.truckPositionFeature);
+
+      layerData.truckInformationMarkerLayer.getSource().removeFeature(this.truckInformationMarkerFeature);
+      this.truckInformationMarkerFeature = this.createTruckInformationMarker(coordinates);
+      layerData.truckInformationMarkerLayer.getSource().addFeature(this.truckInformationMarkerFeature);
+
+      var newTrackFeature = this.createTruckTrackLine(this.currentPosition, coordinates);
+      layerData.truckTrackLayer.getSource().addFeature(newTrackFeature);
+      this.truckTrackFeatures.push(this.createTruckTrackLine(this.currentPosition, coordinates));
+      this.currentPosition = coordinates;
+   };
 
     TruckDataHandler.prototype.createTruckTrackLine = function (prevCoordinates, coordinates) {
         return new ol.Feature({
